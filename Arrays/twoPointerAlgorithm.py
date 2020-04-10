1ca92c9a785e0c591cdac515190920e3bc2e973e
meghnavarma0 <meghnavarma0@gmail.com>
2020-04-10 15:15:31 +0530

    two sum algo

---
+++ Arrays/twoPointerAlgorithm.py
@@ -0,0 +1,25 @@
+# Given an array of integers. Find whether a combination of any two numbers sum up to a given target.
+# eg arr = [-2, 1, 3, 5, 6], target = 4 => True
+
+
+def twoSum(arr, target):
+    arr.sort()
+    i = 0
+    j = len(arr) - 1
+    while i < j:
+        s = arr[i] + arr[j]
+        if s == target:
+            return True
+        if s < target:
+            i += 1
+        else:
+            j += 1
+    return False
+
+
+t = int(input())
+while t:
+    arr = list(map(int, input().split()))
+    target = int(input())
+    print(twoSum(arr, target))
+    t -= 1
