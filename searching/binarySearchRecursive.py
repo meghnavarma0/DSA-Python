ee2334c2d7430380594d461d5a001d9a9c659da7
meghnavarma0 <meghnavarma0@gmail.com>
2020-05-23 14:44:46 +0530

    binary serch recursive

---
+++ searching/binarySearchRecursive.py
@@ -0,0 +1,13 @@
+def binSearch(arr, l, h, x):
+    if l <= h:
+        mid = (l + h)//2
+        if arr[mid] == x:
+            return mid
+        elif x < arr[mid]:
+            return binSearch(arr, l, mid - 1, x)
+        else:
+            return binSearch(arr, mid + 1, h, x)
+    return -1
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(binSearch(arr, 0, len(arr) - 1, 10))
