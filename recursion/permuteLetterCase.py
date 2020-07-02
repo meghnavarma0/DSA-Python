71810576297ce4c8a948b94babd82dea177e59d5
meghnavarma0 <meghnavarma0@gmail.com>
2020-07-02 18:25:02 +0530

    permute letter case

---
+++ recursion/permuteLetterCase.py
@@ -0,0 +1,27 @@
+# Given a string, permute the case of all letter in the string.
+
+def solve(ip, op, result):
+
+    if not ip:
+        result.append("".join(op))
+        return
+
+    if ip[0].isdigit():
+        solve(ip[1:], op[::] + [ip[0]], result)
+
+    elif ip[0].isalpha():
+        op1 = op[::] + [ip[0].lower()]
+        op2 = op[::] + [ip[0].upper()]
+
+        ip = ip[1:]
+        solve(ip, op1, result)
+        solve(ip, op2, result)
+
+if __name__ == "__main__":
+    s = list(input())
+    ip = s[::]
+    op = []
+    result = []
+    solve(ip, op, result)
+    print(" ".join(result))
+
