7a9a081b70a91b40090bf91610b8ffd4950fcbf9
meghnavarma0 <meghnavarma0@gmail.com>
2020-07-27 10:47:44 +0530

    maze and rat problem backtracking

---
+++ backtracking/knightsTourProblem.py
@@ -0,0 +1,38 @@
+n = 8
+def printMatrix(arr, n):
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j], end=" ")
+        print()
+
+def isSafe(arr, x, y):
+    if x >= 0 and x < n and y >= 0 and y < n and arr[x][y] == -1:
+        return True
+    return False
+
+def solve(arr, pos, n, curr_x, curr_y, move_x, move_y):
+    print("a")
+    if pos == n**2:
+        return True
+    for i in range(n):
+        next_x = curr_x + move_x[i]
+        next_y = curr_y + move_y[i]
+        if isSafe(arr, next_x, next_y):
+            arr[next_x][next_y] = pos 
+            if solve(arr, pos+1, n, next_x, next_y, move_x, move_y):
+                return True
+                break
+            arr[next_x][next_y] = -1
+    return False
+
+if __name__ == "__main__":
+    n = 8
+    move_x = [-2, -2, -1, 1, 2, 2, 1, -1]
+    move_y = [1, -1, -2, -2, -1, 1, 2, 2]
+    arr = [[-1 for i in range(n)] for j in range(n)]
+    arr[0][0] = 0
+    pos = 1
+    if not solve(arr, pos, n, 0, 0, move_x, move_y):
+        print("Solution does not exist")
+    else:
+        printMatrix(arr, n)
---
+++ backtracking/ratInAMaze.py
@@ -0,0 +1,41 @@
+# In a boolean matrix, you have to reach from the first to the last block by moving along 1's.If possible, print the solution matrix, else print "NOT POSSIBLE"
+
+
+
+
+n = 4
+maze = [ [1, 0, 0, 0], 
+         [1, 1, 0, 1], 
+         [0, 1, 0, 0], 
+         [1, 1, 1, 1] ] 
+arr = [[0 for i in range(n)] for j in range(n)]
+
+def printMatrix():
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j], end=" ")
+        print()
+
+def isSafe(x, y):
+    if x >= 0 and y >= 0 and x < n and y < n and maze[x][y] == 1:
+        return True
+    return False
+
+def solve(x, y):
+    if x == n-1 and y == n-1 and maze[x][y] == 1:
+        printMatrix()
+        return True
+
+    if isSafe(x, y):
+        arr[x][y] = 1
+        if solve(x+1, y):
+            return True
+    
+        if solve(x, y+1):
+            return True
+        arr[x][y] = 0
+    
+    return False
+
+solve(0, 0)
+
